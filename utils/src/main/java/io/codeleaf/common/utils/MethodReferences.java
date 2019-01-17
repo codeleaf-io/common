c977b34377e723911e2d2da25fa8b90e52b01325
Tom Burger <tvburger@Thomass-MacBook-Air.local>
2019-01-17 16:08:35 +0100

    Adds MethodReferences

---
+++ utils/src/main/java/io/codeleaf/common/utils/MethodReferences.java
@@ -0,0 +1,130 @@
+package io.codeleaf.common.utils;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.*;
+import java.util.function.Supplier;
+
+/**
+ * This class provides method references that can be dereferenced to obtain the referenced method.
+ *
+ * @author tvburger@gmail.com
+ * @since 0.1.0
+ */
+public final class MethodReferences {
+
+    private static final class SetInvokedMethodHandler implements InvocationHandler {
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) {
+            invokedMethod.set(method);
+            Class<?> returnType = method.getReturnType();
+            Object returnValue;
+            if (returnType.isPrimitive()) {
+                switch (returnType.getName()) {
+                    case "boolean":
+                        returnValue = false;
+                        break;
+                    case "byte":
+                        returnValue = (byte) 0x00;
+                        break;
+                    case "char":
+                        returnValue = (char) 0;
+                        break;
+                    case "short":
+                        returnValue = (short) 0;
+                        break;
+                    case "int":
+                        returnValue = 0;
+                        break;
+                    case "long":
+                        returnValue = 0L;
+                        break;
+                    case "float":
+                        returnValue = 0.0f;
+                        break;
+                    case "double":
+                        returnValue = 0.0;
+                        break;
+                    case "void":
+                        returnValue = null;
+                        break;
+                    default:
+                        throw new IllegalStateException("Unknown return type: " + returnType);
+                }
+            } else {
+                returnValue = null;
+            }
+            return returnValue;
+        }
+
+    }
+
+    private static final ThreadLocal<Method> invokedMethod = new ThreadLocal<>();
+    private static final InvocationHandler setInvokedMethodHandler = new SetInvokedMethodHandler();
+    private static final Map<Class<?>, Object> proxyMap = new HashMap<>();
+
+    /**
+     * Creates a proxy object that can be used to create a method reference that can be resolved back to the original method.
+     *
+     * @param typeClass the interface defining the method
+     * @param <T>       the type of the interface
+     * @return a proxy object for the specified interface
+     * @throws IllegalArgumentException if typeClass is not an interface
+     * @throws NullPointerException     if typeClass is <code>null</code>
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T createProxy(Class<T> typeClass) {
+        Objects.requireNonNull(typeClass);
+        if (!typeClass.isInterface()) {
+            throw new IllegalArgumentException("Must be interface!");
+        }
+        List<Class<?>> interfaces = new ArrayList<>();
+        interfaces.add(typeClass);
+        interfaces.addAll(Arrays.asList(typeClass.getInterfaces()));
+        return (T) Proxy.newProxyInstance(typeClass.getClassLoader(), interfaces.toArray(new Class<?>[0]), setInvokedMethodHandler);
+    }
+
+    /**
+     * Returns a proxy object that can be used to create a method reference that can be resolved back to the original method.
+     * If a proxy object for the specific type was already created, it is being reused.
+     *
+     * @param typeClass the interface defining the method
+     * @param <T>       the type of the interface
+     * @return a proxy object for the specified interface
+     * @throws IllegalArgumentException if typeClass is not an interface
+     * @throws NullPointerException     if typeClass is <code>null</code>
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getProxy(Class<T> typeClass) {
+        Objects.requireNonNull(typeClass);
+        return (T) proxyMap.computeIfAbsent(typeClass, MethodReferences::createProxy);
+    }
+
+    /**
+     * Returns the method that was reference by the supplier.
+     *
+     * @param methodReference a method reference created by {@link #reference(Class)}
+     * @return the Method that was referenced
+     * @throws IllegalArgumentException if the methodReference was not using {@link #reference(Class)}
+     * @throws NullPointerException     if methodReference is <code>null</code>
+     */
+    public static Method derefence(Supplier<?> methodReference) {
+        Objects.requireNonNull(methodReference);
+        try {
+            methodReference.get();
+            Method method = invokedMethod.get();
+            if (method == null) {
+                throw new IllegalArgumentException("Could not determine method: methodReference must be obtained using MethodReferences.get(Type.class)::method");
+            }
+            return method;
+        } finally {
+            invokedMethod.remove();
+        }
+    }
+
+    private MethodReferences() {
+    }
+
+}
